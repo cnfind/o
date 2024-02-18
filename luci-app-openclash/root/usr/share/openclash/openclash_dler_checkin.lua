fc7d1bdc1b8b19fa958d35dcd7e46fbbce9695c6
cnfind <cnfind@qq.com>
2024-02-18 10:17:24 +0800

    archived

---
+++ luci-app-openclash/root/usr/share/openclash/openclash_dler_checkin.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/lua
+
+require "nixio"
+require "luci.util"
+require "luci.sys"
+local uci = require("luci.model.uci").cursor()
+local fs = require "luci.openclash"
+local json = require "luci.jsonc"
+
